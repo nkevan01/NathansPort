3f9fc861d717b6bbca7cfbcced66ed82f332067f
nate evans <nkevan01@gmail.com>
2022-11-29 15:50:35 -0800

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,85 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Nathan Evans",
+    "role": "Frontend Developer ",
+    "linkedinId":"Nkevan01",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/nathan-evans-8491611a0/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/nkevan01",
+          "className":"fa fa-github"
+        },
+        
+      ],
+    "aboutme":"I am a self taught Full Stack Web Developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Louisville Ky",
+    "website":"https://github.com/nkevan01",
+    "education":[
+      {
+        "UniversityName":"Self taught Front End Developer",
+        "specialization":"Reactjs",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Toured several coding Bootcamps and landed in the presence of a mentor and took on multiple technologies and became proficient"
+      },
+     
+    ],
+    "work":[
+      {
+        "CompanyName":"Theta Labs",
+        "specialization":"Reactjs Tailwindcss",
+        "MonthOfLeaving":"April",
+        "YearOfLeaving":"",
+        "Achievements":"Colaborated with several other developers to deliver and maintain web apps"
+      },
+      {
+        "CompanyName":"Davidsons Computer Repair llc",
+        "specialization":"Troubleshooting hardware issues",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2020",
+        "Achievements":"Tore down multiple machines and replaced damaged parts in one day"
+      }
+    ],
+    
+    "portfolio":[
+      {
+        "name":"MushyWrld",
+        "description":"web-app",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"Super-chat",
+        "description":"web-app",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
